78b4709dda162b2e8bcc6e7a8b9e1e024667673c
thangngo <thangngo@asnet.com.vn>
2023-11-29 17:41:00 +0700

    Implement header component 40%

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,15 +2,20 @@
 function Header() {
   return (
     <header className="header">
-      <a className="link-title" href="/">
-        <h1 className="header-title">Foods Management</h1>
-      </a>
-      <nav className="header-nav">
-        <div className="nav-heading">
-          <form className="search-form">
-          </form>
-        </div>
-      </nav>
+      <div className="header-wrapper">
+        <a className="link-title" href="/">
+          <h1 className="header-title">Foods Management</h1>
+        </a>
+        <nav className="header-nav">
+          <div className="nav-heading">
+            <form className="search-form">
+            </form>
+          </div>
+        </nav>
+      </div>
+      <div className="header-sub-wrapper">
+
+      </div>
     </header>
   )
 }
