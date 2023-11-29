7de19b9829df8341cda4f1f705dea138b03c1982
thangngo <thangngo@asnet.com.vn>
2023-11-29 14:01:14 +0700

    Init react project for big practice

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="container ">
+      <div className="wrapper">
+        <h1>Hello Word!!!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
