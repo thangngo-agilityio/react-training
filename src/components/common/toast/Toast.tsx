8bf714fbf04ff80bad25ff480afb4b4d677d8d84
david <david.fast1611@gmail.com>
2023-12-01 08:09:37 +0700

    Implement toast component

--- src/components/common/toast/Toast.tsx
+++ src/components/common/toast/Toast.tsx
@@ -1,12 +1,29 @@
+import { useMemo } from "react"
+import { ToastType } from "store/Toast"
+import errorIcon from '../../../assets/icon/icon_error.svg'
+import successIcon from '../../../assets/icon/icon_check.svg'
 
 interface ToastProps {
   message: string
+  isShow?: boolean
+  toastType: ToastType
 }
 
-function Toast({ message }: ToastProps) {
+function Toast({ message, isShow, toastType }: ToastProps) {
+  const toastTypeClass = useMemo(() => toastType.toLowerCase(), [toastType])
+
+  const toastIcon = useMemo(() => {
+    if (toastType === ToastType.Error) {
+      return errorIcon
+    } else {
+      return successIcon
+    }
+  }, [toastType])
+
   return (
-    <div className="toast">
+    <div style={{ visibility: isShow ? 'visible' : 'hidden', opacity: isShow ? '1' : '0' }} className={`toast ${toastTypeClass}`}>
       <div className="toast-body">
+        <img src={toastIcon} alt="icon toast" className='toast-icon' />
         <p className="toast-message">{message}</p>
       </div>
     </div>
---
+++ src/store/Toast.ts
@@ -0,0 +1,4 @@
+export enum ToastType {
+  Success = 'SUCCESS',
+  Error = 'ERROR'
+}
