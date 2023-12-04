28c0e7bb495ecf2d1c8de2b7aaba7eef68b905a6
thangngo <thangngo@asnet.com.vn>
2023-12-04 17:00:15 +0700

    implement handle click onsubmit for modal create product

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,7 +5,7 @@ import { defaultData } from "constants/food"
 import AddCard from "@components/common/card/addCard/AddCard"
 import Spinner from "@components/common/spinner/Spinner"
 import MultiModal from "@components/modals/multiModal/MultiModal"
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { Product } from "interfaces/product/Product"
 import { mutationProduct } from "service/product"
 import useProduct from "@components/hooks/useProduct"
@@ -15,6 +15,7 @@ function MainPage() {
   const [modalProductData, setModalProductData] = useState(defaultData)
 
   const { productData } = useProduct()
+  const queryClient = useQueryClient()
 
   const {
     mutationModal,
@@ -33,25 +34,35 @@ function MainPage() {
     },
 
     onSuccess: () => {
+      onCancelModal()
       setLoadingShowUp(false)
     },
     onError: () => {
+      onCancelModal()
       setLoadingShowUp(false)
-    }
+    },
+
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['products'] })
+    },
+
+    networkMode: 'always'
   })
 
   const onClickAdd = useCallback(() => {
     setMutationShowUp(true, MODAL_TITLE.ADD, defaultData)
   }, [setMutationShowUp])
 
+  // submit modal
   const onSubmit = useCallback((e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     mutateProduct(modalProductData)
   }, [mutateProduct, modalProductData])
 
+  // Cancel modal
   const onCancelModal = useCallback(() => {
-    if (modalProductData.id === defaultData.id) {
+    if (modalProductData?.id === defaultData.id) {
       setModalProductData(defaultData);
     } else {
       setModalProductData(mutationModal.productData!)
