41c9a94eeec78c2d68a8774f061f2b46aeeac60d
thangngo <thangngo@asnet.com.vn>
2023-12-05 09:34:31 +0700

    Implement show loading page

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,28 @@
-import { ReactNode } from "react"
+import { ReactNode, useContext } from "react"
 import Header from "@components/header/Header"
+import { ModalContext } from "context/modal"
+import Spinner from "@components/common/spinner/Spinner"
 
 interface LayoutProps {
   children: ReactNode
 }
 
 function Layout({ children }: LayoutProps) {
+
+  const { loadingShowUp } = useContext(ModalContext)
+
   return (
-    <div className="container">
-      <div className="wrapper">
-        <Header />
-        {children}
+    <>
+      <div className="container">
+        <div className="wrapper">
+          <Header />
+          {children}
+        </div>
       </div>
-    </div>
+      {loadingShowUp && (
+        <Spinner />
+      )}
+    </>
   )
 }
 
