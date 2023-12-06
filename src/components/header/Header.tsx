6554619a990aeef917551e312f0ecb4235c257aa
thangngo <thangngo@asnet.com.vn>
2023-12-06 17:57:16 +0700

    Implement handle search product 30%

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,22 @@
 import InputField from "@components/common/inputField/InputField"
 import Sort from "@components/common/sort/Sort"
 import iconSearch from '../../../src/assets/icon/icon_search.svg'
+import { useEffect, useState } from "react"
+import useProduct from "@components/hooks/useProduct"
 
 function Header() {
+  const [searchInput, setSearchInput] = useState('')
+  const { refetch } = useProduct()
+
+  useEffect(() => {
+    refetch()
+    setSearchInput(searchInput)
+  }, [searchInput, refetch])
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value)
+  }
+
   return (
     <header className="header">
       <a className="link-title" href="/">
@@ -12,7 +26,7 @@ function Header() {
         <div className="nav-heading">
           <form className="form-search">
             <img src={iconSearch} className="icon-search" loading="eager" alt="search" />
-            <InputField type="search" inputClass="input-search" htmlFor="search" placeholder="Search for food, coffe, etc.." />
+            <InputField type="search" inputClass="input-search" htmlFor="search" placeholder="Search for food, coffe, etc.." value={searchInput} onChange={handleSearch} />
           </form>
         </div>
         <Sort selectOptions={
